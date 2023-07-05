9898abc969de8444f6ec1ab22ef8365dacbc43aa
Ali Chaabane <alichaabane98@gmail.com>
2023-07-05 09:29:36 +0100

    init project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularCypressTuto'`, () => {
+  it(`should have as title 'angular-cypress'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularCypressTuto');
+    expect(app.title).toEqual('angular-cypress');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularCypressTuto app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-cypress app is running!');
   });
 });
